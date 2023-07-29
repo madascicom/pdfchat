08ff0652fa0a51b925f898095c7e215ace6d11b9
madascicom <137607688+madascicom@users.noreply.github.com>
2023-07-29 19:01:48 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -105,5 +105,5 @@ if user_query:
     with st.chat_message("assistant"):
         retrieval_handler = PrintRetrievalHandler(st.container())
         stream_handler = StreamHandler(st.empty())
-        response = qa_chain.run(user_query, callbacks=[stream_handler])
+        response = qa_chain.run(user_query, callbacks=[stream_handler, retrieval_handler])
         st.write(response)
